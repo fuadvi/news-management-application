ae970c2e9de085068f8bfff663058d0c049f58c6
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-08-10 23:51:04 +0700

    feat seeder: create user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'id' => 1,
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('password'),
+                'role_id' => 1,
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
