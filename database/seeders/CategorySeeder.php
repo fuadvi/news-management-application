19a37ff68e595f7b2c3a16fb8ce91be61068a2cc
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-08-10 23:56:46 +0700

    feat seeder: create category seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            [
+                'id' => 1,
+                'name' => 'Teknologi',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Bisnis',
+            ],
+            [
+                'id' => 3,
+                'name' => 'Hiburan',
+            ],
+            [
+                'id' => 4,
+                'name' => 'Kesehatan',
+            ],
+            [
+                'id' => 5,
+                'name' => 'Olahraga',
+            ],
+        ];
+    }
+}
