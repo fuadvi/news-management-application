f682c248145de5669d35c77d499a3fe4ec1c81ba
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-08-11 16:22:08 +0700

    feat login: create feature login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Dtos\AuthDto;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\ReqisterRequest;
 use App\Repository\User\IUserRepository;
 use App\Traits\ResponseAPI;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -21,6 +23,33 @@ class AuthController extends Controller
         try {
             $user = $this->userRepo->createUser(AuthDto::fromRequestRegister($request));
             $token = $user->createToken('auth_token')->accessToken;
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage());
+        }
+
+        return $this->success(
+            'User created',
+            [
+                "user" => $user,
+                "token" => $token
+            ]
+        );
+    }
+
+    public function login(LoginRequest $request)
+    {
+        try {
+            $user = $this->userRepo->getUserByEmail($request->email);
+
+            if (!$user)  return $this->error('User not found');
+
+            if(!Hash::check($request->password, $user->password)) return $this->error('User not found');
+
+            match ($user->role_id) {
+                1 => $token = $user->createToken('AdminToken', ['admin'])->accessToken,
+                default => $token = $user->createToken('UserToken', ['user'])->accessToken,
+            };
+
         } catch (\Exception $e) {
             return $this->error($e->getMessage());
         }
