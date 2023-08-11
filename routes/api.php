c6ad957a85643c0cb09afa5738e2f6d5cd9cdc17
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-08-11 16:23:10 +0700

    feat: refactor route register to use route controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:api','is.admin'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::controller(AuthController::class)
+->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/register', 'register');
+});
+
