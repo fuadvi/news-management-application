1347652a0180e1962695da1116b2f477f113875b
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-08-12 16:30:16 +0700

    fix: bug http code

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
             };
 
         } catch (\Exception $e) {
-            return $this->error($e->getMessage());
+            return $this->error($e->getMessage(), 400);
         }
 
         return $this->success(
